2e95a422596fff097706d5ed026381aec8975bc4
Daniel <danleavitt0@gmail.com>
2015-06-05 16:24:30 -0700

    fixed dependencies

--- user.js
+++ user.js
@@ -1,3 +1,11 @@
-//code goes here
+var userInfo = {
+	name:'Daniel',
+	username:'danleavitt0',
+	age:25
+}
+
+function getUser(user)  {
+	console.log(user.name + ' aka ' + user.username + ' is ' + age + ' years old.')
+}
 
 document.body.innerHTML = "Open your browser console!"
